ec1edc42b6c3beecf3d599778fc9664fc202a44c
brightmanforever022 <brightmanforever@outlook.com>
2020-11-04 23:32:19 +0800

    change the part for db connection

--- database/db.js
+++ database/db.js
@@ -8,11 +8,27 @@ const dbConfig = require('../config/db.config.js');
 //   database: dbConfig.database
 // });
 
-const connection = mysql.createConnection(dbConfig.db_uri);
+let connection;
 
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig.db_uri);
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection
