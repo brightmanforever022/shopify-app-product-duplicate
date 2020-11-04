28f7143d8906bbc4ea1bea2f4138ac3eee4f390f
brightmanforever022 <brightmanforever@outlook.com>
2020-11-04 23:22:27 +0800

    change the settings for db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  db_uri: process.env.CLEARDB_DATABASE_URL,
 }
--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
 
 const dbConfig = require('../config/db.config.js');
-const connection = mysql.createConnection({
-  host: dbConfig.host,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database
-});
+// const connection = mysql.createConnection({
+//   host: dbConfig.host,
+//   user: dbConfig.user,
+//   password: dbConfig.password,
+//   database: dbConfig.database
+// });
+
+const connection = mysql.createConnection(dbConfig.db_uri);
 
 connection.connect(error => {
   if (error) throw error;
